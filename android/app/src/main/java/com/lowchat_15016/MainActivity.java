b564087138c927448e7d3e7af1e986dca9373ac5
Crowdbotics <team@crowdbotics.com>
2020-03-24 09:18:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lowchat_15016/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lowchat_15016;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "lowchat_15016";
+    }
+}
